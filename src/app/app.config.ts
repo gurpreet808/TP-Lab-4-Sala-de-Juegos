7b62939b04d493e6f993e6ca0dd49fbbb37c3037
gurpreet <gurpreets808@gmail.com>
2024-04-25 15:45:04 -0300

    Agrego Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,45 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(
+        () => initializeApp(
+          {
+            "projectId": "lab4-saladejuegos",
+            "appId": "1:1091172030495:web:45a316f2f560a782235d2a",
+            "storageBucket": "lab4-saladejuegos.appspot.com",
+            "apiKey": "AIzaSyB9ijQYN7qGGqeOrLjuNwCaVx1hrUYokcQ",
+            "authDomain": "lab4-saladejuegos.firebaseapp.com",
+            "messagingSenderId": "1091172030495"
+          }
+        )
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    UserTrackingService,
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+    importProvidersFrom(provideMessaging(() => getMessaging())),
+    importProvidersFrom(providePerformance(() => getPerformance())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+    importProvidersFrom(provideRemoteConfig(() => getRemoteConfig())), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "lab4-saladejuegos", "appId": "1:1091172030495:web:45a316f2f560a782235d2a", "storageBucket": "lab4-saladejuegos.appspot.com", "apiKey": "AIzaSyB9ijQYN7qGGqeOrLjuNwCaVx1hrUYokcQ", "authDomain": "lab4-saladejuegos.firebaseapp.com", "messagingSenderId": "1091172030495" }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideFunctions(() => getFunctions())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(providePerformance(() => getPerformance())), importProvidersFrom(provideStorage(() => getStorage())), importProvidersFrom(provideRemoteConfig(() => getRemoteConfig()))
+  ]
 };
