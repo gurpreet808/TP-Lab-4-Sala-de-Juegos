ddb1541ac1625f8890a29d665e5b06976325fdcb
gurpreet <gurpreets808@gmail.com>
2024-04-25 17:45:30 -0300

    Agrego estructura y estilos a quien soy

--- src/app/componentes/quien-soy/quien-soy.component.ts
+++ src/app/componentes/quien-soy/quien-soy.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './quien-soy.component.scss'
 })
 export class QuienSoyComponent {
-
+  url_foto: string = "https://avatars.githubusercontent.com/u/17884026";
+  nombre: string = 'Daniel Singh';
 }
