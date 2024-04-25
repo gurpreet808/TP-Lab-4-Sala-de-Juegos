5954179b9b7ed377db29dec820c4646651363523
gurpreet <gurpreets808@gmail.com>
2024-04-25 17:45:48 -0300

    Agrego login component

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  correo: string = '';
+  clave: string = '';
+
+  constructor(public router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.router.navigate(['/']);
+  }
+
+  olvideClave() {
+
+  }
 
 }
